15fa1aafe4e11a328278e9b71c7255f2f8c1f748
erezabegu <ereza@dua.com>
2023-04-22 23:20:53 +0200

    testest

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
     return (
         <div className="App">
-            <h1>Test project to implement CI github actionsssss</h1>
+            <h1>Test project to implement CI github action</h1>
         </div>
     )
 }
