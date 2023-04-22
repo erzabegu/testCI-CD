d88e2793f3ace01cfb6514d45b41d710e21cb80c
erezabegu <ereza@dua.com>
2023-04-22 22:53:39 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+    return (
+        <div className="App">
+            <h1>Test project to implement CI github actions</h1>
+        </div>
+    )
+}
+
+export default App
